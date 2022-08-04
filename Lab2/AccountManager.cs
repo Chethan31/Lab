5c0da5a1012cfb54796c2b272734d98596cf1ba0
Chethan Gowda <chethanrgowda38@gmail.com>
2022-08-04 12:25:21 +0530

    Add project files.

---
+++ Lab2/AccountManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab2
+{
+    class AccountManager
+    {
+        /// <summary>
+        /// Method to store account details into the account object passed
+        /// </summary>
+        /// <param name="Acc"></param>
+        public void FillAccountData(Account Acc)
+        {
+            Acc._AccNo = "004701";
+            Acc._Name = "Nitesh";
+            Acc._Balance = 45000.0;
+        }
+
+        /// <summary>
+        /// Method to display account details from the account object passed
+        /// </summary>
+        /// <param name="Acc"></param>
+        public void DisplayAccountData(Account Acc)
+        {
+            //Write code to display the Account
+            //object
+            Console.WriteLine("AccNo : " + Acc._AccNo);
+            Console.WriteLine("Name : " + Acc._Name);
+            Console.WriteLine("Balance : " + Acc._Balance);
+        }
+    }
+}
---
+++ Lab3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Emp = new Employee();
+
+            StoreData(Emp);
+
+            ShowData(Emp);
+        }
+
+        static void StoreData(Employee Emp)
+        {
+            Emp.EmployeeId = int.Parse(Console.ReadLine());
+            Emp.Name= Console.ReadLine();
+            Emp.Gender=char.Parse(Console.ReadLine());
+            Address address = new Address();    
+            address.Address1 = Console.ReadLine();
+            address.Address2 = Console.ReadLine();
+            address.City = Console.ReadLine();
+            address.PinCode = int.Parse(Console.ReadLine());
+            Emp.EmpAddress = address;
+        }
+
+        static void ShowData(Employee Emp)
+        {
+            //----------------Display the employee information
+            Console.WriteLine($"Employee Id :{Emp.EmployeeId} ");
+            Console.WriteLine($"Employee Name :{Emp.Name} ");
+            Console.WriteLine($"Employee Gender :{Emp.Gender}");
+
+            Console.WriteLine("Employee Address : --------------");
+            Console.WriteLine($"Address 1 : {Emp.EmpAddress.Address1}");
+           Console.WriteLine($"Address 2 : {Emp.EmpAddress.Address2}");
+            Console.WriteLine($"City :  {Emp.EmpAddress.City}");
+           Console.WriteLine($"PinCode :{Emp.EmpAddress.PinCode} ");
+            Console.WriteLine("----------------------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
